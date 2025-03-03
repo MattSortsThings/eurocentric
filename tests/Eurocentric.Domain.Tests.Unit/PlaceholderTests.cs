7cda0d5205cd1cc5a9bf2738352a025d71bd3dd7
Matt Tantony <matt.tantony@gmail.com>
2025-03-03 10:04:53 +0000

    test: add unit test assemblies
    Added unit test assemblies for the Eurocentric.Domain and
    Eurocentric.Shared assemblies. They currently contain placeholder tests
    that always pass.
    
    Closes: #44

---
+++ tests/Eurocentric.Domain.Tests.Unit/PlaceholderTests.cs
@@ -0,0 +1,12 @@
+using Eurocentric.Domain.Tests.Unit.Utils;
+
+namespace Eurocentric.Domain.Tests.Unit;
+
+public static class PlaceholderTests
+{
+    public sealed class Placeholder : UnitTest
+    {
+        [Fact]
+        public void Should_always_pass() => Assert.True(true);
+    }
+}
