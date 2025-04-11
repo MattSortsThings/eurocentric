26cb8a55ba188d6be3bcf678ee12a3b13c95d1dd
Matt Tantony <matt.tantony@gmail.com>
2025-04-11 13:11:30 +0100

    test: update architecture tests

--- tests/architecture/Eurocentric.Features.ArchitectureTests/FeaturesAssemblyTests.cs
+++ tests/architecture/Eurocentric.Features.ArchitectureTests/FeaturesAssemblyTests.cs
@@ -1,8 +1,8 @@
 using ArchUnitNET.Domain;
 using ArchUnitNET.Domain.Extensions;
-using ArchUnitNET.Fluent.Predicates;
 using ArchUnitNET.Loader;
 using ArchUnitNET.xUnitV3;
+using Eurocentric.Features.Shared.Messaging;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
 
 namespace Eurocentric.Features.ArchitectureTests;
@@ -14,18 +14,78 @@ public sealed class FeaturesAssemblyTests
         .LoadAssembly(typeof(Middleware).Assembly)
         .Build();
 
+    private static readonly IObjectProvider<IType> TypesInAdminApiNamespace = Types().That()
+        .FollowCustomPredicate(t => t.Namespace.NameContains("AdminApi"), "Namespace contains AdminApi");
+
+    private static readonly IObjectProvider<IType> TypesInPublicApiNamespace = Types().That()
+        .FollowCustomPredicate(t => t.Namespace.NameContains("PublicApi"), "Namespace contains PublicApi");
+
+    private static readonly IObjectProvider<Class> ResponseClasses = Classes().That().HaveNameEndingWith("Response");
+
+    private static readonly IObjectProvider<Class> CommandClasses = Classes().That().HaveNameEndingWith("Command");
+
+    private static readonly IObjectProvider<Class> QueryClasses = Classes().That().HaveNameEndingWith("Query");
+
+    private static readonly IObjectProvider<Class> CommandHandlerClasses = Classes().That().HaveNameEndingWith("CommandHandler");
+
+    private static readonly IObjectProvider<Class> QueryHandlerClasses = Classes().That().HaveNameEndingWith("QueryHandler");
+
     [Fact]
-    [Trait("Category", "Placeholder")]
-    public void Should_always_pass() => Classes()
-        .That().FollowCustomPredicate(new ModelsPredicate())
-        .Should().BeSealed()
+    public void Types_in_AdminApi_namespace_should_not_depend_on_types_in_PublicApi_namespace() => Types()
+        .That().Are(TypesInAdminApiNamespace)
+        .Should().NotDependOnAnyTypesThat().Are(TypesInPublicApiNamespace)
         .Check(Architecture);
 
-    private class ModelsPredicate : IPredicate<Class>
-    {
-        public IEnumerable<Class> GetMatchingObjects(IEnumerable<Class> objects, Architecture architecture) =>
-            objects.Where(@class => @class.Namespace.NameEndsWith(".Models"));
+    [Fact]
+    public void Types_in_PublicApi_namespace_should_not_depend_on_types_in_AdminApi_namespace() => Types()
+        .That().Are(TypesInPublicApiNamespace)
+        .Should().NotDependOnAnyTypesThat().Are(TypesInAdminApiNamespace)
+        .Check(Architecture);
 
-        public string Description => "namespace ends with '.Models'";
-    }
+    [Fact]
+    public void Response_classes_should_be_public_sealed_non_nested_records() => Classes()
+        .That().Are(ResponseClasses)
+        .Should().BePublic()
+        .AndShould().BeSealed()
+        .AndShould().NotBeNested()
+        .AndShould().BeRecord()
+        .Check(Architecture);
+
+    [Fact]
+    public void Command_classes_should_extend_generic_Request_class_and_be_public_sealed_and_not_nested() => Classes()
+        .That().Are(CommandClasses)
+        .Should().BeAssignableTo(typeof(Request<>))
+        .AndShould().BePublic()
+        .AndShould().BeSealed()
+        .AndShould().NotBeNested()
+        .Check(Architecture);
+
+    [Fact]
+    public void Query_classes_should_extend_generic_Request_class_and_be_public_sealed_and_not_nested() => Classes()
+        .That().Are(QueryClasses)
+        .Should().BeAssignableTo(typeof(Request<>))
+        .AndShould().BePublic()
+        .AndShould().BeSealed()
+        .AndShould().NotBeNested()
+        .Check(Architecture);
+
+    [Fact]
+    public void CommandHandler_classes_should_extend_generic_RequestHandler_class_and_be_internal_sealed_and_not_nested() =>
+        Classes()
+            .That().Are(CommandHandlerClasses)
+            .Should().BeAssignableTo(typeof(RequestHandler<,>))
+            .AndShould().BeInternal()
+            .AndShould().BeSealed()
+            .AndShould().NotBeNested()
+            .Check(Architecture);
+
+    [Fact]
+    public void QueryHandler_classes_should_extend_generic_RequestHandler_class_and_be_internal_sealed_and_not_nested() =>
+        Classes()
+            .That().Are(QueryHandlerClasses)
+            .Should().BeAssignableTo(typeof(RequestHandler<,>))
+            .AndShould().BeInternal()
+            .AndShould().BeSealed()
+            .AndShould().NotBeNested()
+            .Check(Architecture);
 }
