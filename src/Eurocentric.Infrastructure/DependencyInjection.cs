32abac84c99230d3ac5d162fde9ac86a04ee1773
Matt Tantony <matt.tantony@gmail.com>
2025-07-06 07:29:42 +0100

    chore: add assembly architecture
    Implemented the assembly architecture as outlined in the system design
    decisions dev note.
    
    Closes: #48

---
+++ src/Eurocentric.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eurocentric.Infrastructure;
+
+/// <summary>
+///     Extension methods to be invoked at the application composition root.
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    ///     Adds the <see cref="Eurocentric.Infrastructure" /> assembly services to the application service descriptor
+    ///     collection.
+    /// </summary>
+    /// <param name="services">Contains service descriptors for the application.</param>
+    /// <param name="configuration">Contains configuration properties for the application.</param>
+    /// <returns>The same <see cref="IServiceCollection" /> instance, so that method invocations can be chained.</returns>
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) =>
+        services;
+}
---
+++ src/Eurocentric.WebApp/Startup.cs
@@ -0,0 +1,37 @@
+using Eurocentric.Features;
+using Eurocentric.Infrastructure;
+
+namespace Eurocentric.WebApp;
+
+/// <summary>
+///     Extension methods to be invoked on application startup.
+/// </summary>
+internal static class Startup
+{
+    /// <summary>
+    ///     Configures all the services for the web application to be built.
+    /// </summary>
+    /// <param name="builder">The web application builder.</param>
+    /// <returns>The same <see cref="WebApplicationBuilder" /> instance, so that method invocations can be chained.</returns>
+    internal static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddInfrastructureServices(builder.Configuration).AddFeaturesServices();
+
+        return builder;
+    }
+
+    /// <summary>
+    ///     Configures the HTTP request pipeline middleware for the web application.
+    /// </summary>
+    /// <param name="app">The web application.</param>
+    /// <returns>The same <see cref="WebApplication" /> instance, so that method invocations can be chained.</returns>
+    internal static WebApplication ConfigureMiddleware(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+
+        app.MapGet("/placeholder", () => TypedResults.Ok("You don't have to tell me twice! But during the Stone Age..."))
+            .AllowAnonymous();
+
+        return app;
+    }
+}
