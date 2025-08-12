b7e96ac839815277cd576201dc21eb8abf9ae50e
Matt Tantony <matt.tantony@gmail.com>
2025-08-12 17:25:22 +0100

    test: remove placeholder acceptance tests
    Deleted all acceptance tests for Public API v0.x placeholder endpoints,
    as these are now redundant.

--- tests/acceptance/Eurocentric.Features.AcceptanceTests/PublicApi/V0/Utils/ParallelSeededAcceptanceTest.cs
+++
@@ -1,8 +0,0 @@
-namespace Eurocentric.Features.AcceptanceTests.PublicApi.V0.Utils;
-
-[Category("acceptance")]
-public abstract class ParallelSeededAcceptanceTest
-{
-    [ClassDataSource<SeededWebAppFixture>(Shared = SharedType.PerAssembly)]
-    public required SeededWebAppFixture SystemUnderTest { get; init; }
-}
