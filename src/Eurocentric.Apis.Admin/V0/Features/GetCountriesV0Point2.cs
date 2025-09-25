af0b650e5fefb0749c240f7e79323f99fc45c97f
Matt Tantony <matt.tantony@gmail.com>
2025-09-25 08:42:38 +0100

    feat(admin-api): define v0.2
    Defined version 0.2 of the Admin API. This has 3 endpoints.
    
    Closes: #47

---
+++ src/Eurocentric.Apis.Admin/V0/Features/GetCountriesV0Point2.cs
@@ -0,0 +1,73 @@
+using ErrorOr;
+using Eurocentric.Apis.Admin.V0.Constants;
+using Eurocentric.Apis.Admin.V0.Contracts.Countries;
+using Eurocentric.Apis.Admin.V0.Contracts.Mapping;
+using Eurocentric.Domain.V0.Aggregates.Countries;
+using Eurocentric.Infrastructure.DataAccess.EfCore;
+using Eurocentric.Infrastructure.Messaging;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using SlimMessageBus;
+
+namespace Eurocentric.Apis.Admin.V0.Features;
+
+internal static class GetCountriesV0Point2
+{
+    internal static IEndpointRouteBuilder MapGetCountriesV0Point2(
+        this IEndpointRouteBuilder builder
+    )
+    {
+        builder
+            .MapGet("v0.2/countries", ExecuteAsync)
+            .WithName("AdminApi.V0.Countries.GetCountriesV0Point2")
+            .WithTags(V0Group.Countries.Tag)
+            .Produces<GetCountriesResponse>();
+
+        return builder;
+    }
+
+    private static async Task<IResult> ExecuteAsync(
+        [FromServices] IRequestResponseBus bus,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ErrorOr<Result> errorsOrResult = await bus.Send(
+            new Query(),
+            cancellationToken: cancellationToken
+        );
+
+        return TypedResults.Ok(errorsOrResult.Value);
+    }
+
+    private static Ok<GetCountriesResponse> MapToOk(in Result result)
+    {
+        GetCountriesResponse response = new(
+            result.Countries.Select(country => country.ToCountryDto()).ToArray()
+        );
+
+        return TypedResults.Ok(response);
+    }
+
+    internal readonly record struct Result(Country[] Countries);
+
+    internal sealed record Query : IQuery<Result>;
+
+    [UsedImplicitly]
+    internal sealed class QueryHandler(AppDbContext dbContext) : IQueryHandler<Query, Result>
+    {
+        public async Task<ErrorOr<Result>> OnHandle(Query _, CancellationToken cancellationToken)
+        {
+            Country[] countries = await dbContext
+                .Countries.AsNoTracking()
+                .OrderBy(country => country.CountryCode)
+                .ToArrayAsync(cancellationToken);
+
+            return new Result(countries);
+        }
+    }
+}
--- src/Eurocentric.Apis.Admin/V0/Middleware.cs
+++ src/Eurocentric.Apis.Admin/V0/Middleware.cs
@@ -12,6 +12,10 @@ public static class Middleware
     ///     Maps the version 0.x endpoints to the endpoint route builder.
     /// </summary>
     /// <param name="builder">The endpoint route builder.</param>
-    public static void MapV0Endpoints(this IEndpointRouteBuilder builder) =>
+    public static void MapV0Endpoints(this IEndpointRouteBuilder builder)
+    {
         builder.MapCreateCountryV0Point1().MapGetCountryV0Point1();
+
+        builder.MapCreateCountryV0Point2().MapGetCountriesV0Point2().MapGetCountryV0Point2();
+    }
 }
