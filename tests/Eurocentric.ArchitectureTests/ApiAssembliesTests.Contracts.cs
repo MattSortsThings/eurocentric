2aea25758db9a63057546ae239c1b1aacf875abd
Matt Tantony <matt.tantony@gmail.com>
2025-10-27 13:58:23 +0000

    test: add architecture tests

---
+++ tests/Eurocentric.ArchitectureTests/ApiAssembliesTests.Contracts.cs
@@ -0,0 +1,106 @@
+namespace Eurocentric.ArchitectureTests;
+
+public sealed partial class ApiAssembliesTests
+{
+    private static readonly IObjectProvider<Class> DtoClasses = Classes()
+        .That()
+        .Are(Types().That().Are(AdminApiAssemblyTypes).Or().Are(PublicApiAssemblyTypes))
+        .And()
+        .ResideInNamespaceMatching("Eurocentric.Apis.*.*.Dtos.")
+        .And()
+        .AreNotAbstract();
+
+    private static readonly IObjectProvider<Class> FeatureRequestClasses = Classes()
+        .That()
+        .Are(Types().That().Are(AdminApiAssemblyTypes).Or().Are(PublicApiAssemblyTypes))
+        .And()
+        .ResideInNamespaceMatching("Eurocentric.Apis.*.*.Features.*.")
+        .And()
+        .HaveNameEndingWith("Request");
+
+    private static readonly IObjectProvider<Class> FeatureResponseClasses = Classes()
+        .That()
+        .Are(Types().That().Are(AdminApiAssemblyTypes).Or().Are(PublicApiAssemblyTypes))
+        .And()
+        .ResideInNamespaceMatching("Eurocentric.Apis.*.*.Features.*.")
+        .And()
+        .HaveNameEndingWith("Response");
+
+    [Test]
+    public async Task DTO_classes_should_be_public()
+    {
+        // Arrange
+        ClassRule rule = Classes().That().Are(DtoClasses).Should().BePublic();
+
+        // Act
+        IEnumerable<EvaluationResult> result = rule.Evaluate(ArchitectureUnderTest);
+
+        // Assert
+        await Assert.That(result).ContainsOnly(Passed);
+    }
+
+    [Test]
+    public async Task DTO_classes_should_be_immutable_records()
+    {
+        // Arrange
+        ClassRule rule = Classes().That().Are(DtoClasses).Should().BeImmutable().AndShould().BeRecord();
+
+        // Act
+        IEnumerable<EvaluationResult> result = rule.Evaluate(ArchitectureUnderTest);
+
+        // Assert
+        await Assert.That(result).ContainsOnly(Passed);
+    }
+
+    [Test]
+    public async Task Feature_request_classes_should_be_public()
+    {
+        // Arrange
+        ClassRule rule = Classes().That().Are(FeatureRequestClasses).Should().BePublic();
+
+        // Act
+        IEnumerable<EvaluationResult> result = rule.Evaluate(ArchitectureUnderTest);
+
+        // Assert
+        await Assert.That(result).ContainsOnly(Passed);
+    }
+
+    [Test]
+    public async Task Feature_request_classes_should_be_immutable_records()
+    {
+        // Arrange
+        ClassRule rule = Classes().That().Are(FeatureRequestClasses).Should().BeImmutable().AndShould().BeRecord();
+
+        // Act
+        IEnumerable<EvaluationResult> result = rule.Evaluate(ArchitectureUnderTest);
+
+        // Assert
+        await Assert.That(result).ContainsOnly(Passed);
+    }
+
+    [Test]
+    public async Task Feature_response_classes_should_be_public()
+    {
+        // Arrange
+        ClassRule rule = Classes().That().Are(FeatureResponseClasses).Should().BePublic();
+
+        // Act
+        IEnumerable<EvaluationResult> result = rule.Evaluate(ArchitectureUnderTest);
+
+        // Assert
+        await Assert.That(result).ContainsOnly(Passed);
+    }
+
+    [Test]
+    public async Task Feature_response_classes_should_be_immutable_records()
+    {
+        // Arrange
+        ClassRule rule = Classes().That().Are(FeatureResponseClasses).Should().BeImmutable().AndShould().BeRecord();
+
+        // Act
+        IEnumerable<EvaluationResult> result = rule.Evaluate(ArchitectureUnderTest);
+
+        // Assert
+        await Assert.That(result).ContainsOnly(Passed);
+    }
+}
