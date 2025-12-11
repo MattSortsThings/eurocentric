b8ab4a5d7f5fd394ea42bf2bd2a93c3a0f96ae34
Matt Tantony <matt.tantony@gmail.com>
2025-12-11 08:46:59 +0000

    feat: add domain error type enum
    Added a `DomainErrorType` enum to the `Eurocentric.Domain.Enums`
    namespace.
    
    Refs: #65

---
+++ src/Eurocentric.Domain/Enums/DomainErrorType.cs
@@ -0,0 +1,28 @@
+namespace Eurocentric.Domain.Enums;
+
+/// <summary>
+///     Specifies a domain error's type.
+/// </summary>
+public enum DomainErrorType
+{
+    /// <summary>
+    ///     The error was unexpected.
+    /// </summary>
+    Unexpected,
+
+    /// <summary>
+    ///     The error arose because the requested aggregate was not found.
+    /// </summary>
+    NotFound,
+
+    /// <summary>
+    ///     The error arose because the request was impossible given the current state of all existing aggregates.
+    /// </summary>
+    Conflict,
+
+    /// <summary>
+    ///     The error arose because the request was impossible, irrespective of the current state of all existing
+    ///     aggregates.
+    /// </summary>
+    Unprocessable,
+}
